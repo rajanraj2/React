af5441679e5db5dfe51f1592ce1aabc0604079a9
BabyMonk <rans2203@gmail.com>
2024-04-11 17:02:36 +0530

    Add routing

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,14 +1,33 @@
 import MyHeading, { MyheadingLittle3 as MyCustomName, MyheadingLittle4 } from "./components/Myheading";
+import Header from "./components/Header";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Product from "./components/Product";
+
+import './styles/App.css';
+
+
 
 function App() {
   return (
     <>
-    <div>
-      <MyHeading name="Rajan" text="Nice one" price="23435"/>
-      <MyCustomName />
-      <MyheadingLittle4 />
-      <p> This is a paragraph.</p>
-    </div>
+    <Router>
+
+      < Header />
+
+      <Routes>
+
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/contact" element={<Contact/>} />
+        {/* <Route path="/product/:id" element={<Product/>} /> */}
+        <Route path="*" element={<h1>Page Not Found 404</h1>} />
+
+      </Routes>
+    
+    </Router>
     </>
   )
 }
